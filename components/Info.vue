01a642f6580488441a8ebeacb691f622bccaa85e
coderz <coderz@gmail.com>
2022-09-13 20:16:47 -0600

    Title: Change and added some code and file

---
+++ components/Info.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="info-container">
+    <h2 ref="txt">Hi</h2>
+  </div>
+</template>
+<script>
+export default {
+  data(){
+    return {
+      scp: 0,
+    }
+  },
+
+  mounted(){
+    window.onscroll = ()=> {
+      this.scp = window.pageYOffset;
+    }
+  },
+
+  // Component name
+  name: "info",
+}
+</script>
